573f9af765fdf76e5bbe137092a61dc639bb086f
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-08-10 14:20:39 +0300

    Update

---
+++ C++/leetcode_Algorithms/Palindrome_Number.cpp
@@ -0,0 +1,28 @@
+/*SOLUTİON: 
+
+//https://leetcode.com/problems/palindrome-number/description/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+        return false;
+        }
+        long long secondnum=0; 
+        int tx = x;
+        while(tx>0){
+            int temp = tx % 10;
+            tx= tx/10;
+            secondnum = secondnum * 10 + temp;
+            
+        }
+        if(secondnum == x ){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+*/
