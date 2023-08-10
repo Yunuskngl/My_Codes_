7f8ef7325c46a687e4ebcbeb75f5382eb94ab092
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-08-10 03:25:56 +0300

    Update

---
+++ C++/leetcode_Algorithms/Best_Time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,21 @@
+/*SOLUTİON:
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int maxprices = 0;
+        int minprices = prices[0];
+        int sell = 0;
+        for(int j = 0 ; j < prices.size() ; j++){
+            minprices = min(minprices,prices[j]);
+            sell = prices[j] - minprices;
+            maxprices = max(maxprices,sell);
+        }
+        return maxprices;
+    }
+};
+
+*/
