5e3f0feb3c2972220b67330b24d89f7c168724b8
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-08-10 16:19:15 +0300

    Update

---
+++ C++/leetcode_Algorithms/Roman_to_Integer.cpp
@@ -0,0 +1,51 @@
+/*SOLUTİON:
+
+//https://leetcode.com/problems/roman-to-integer/description/
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int sum = 0; 
+        for(int i = s.size() - 1; i >= 0; i--) {
+            if(i > 0) { 
+                if(s[i] == 'V' && s[i-1] == 'I') {
+                    sum += 4;
+                    i--;
+                    continue;
+                } else if(s[i] == 'X' && s[i-1] == 'I') {
+                    sum += 9;
+                    i--;
+                    continue;
+                } else if(s[i] == 'C' && s[i-1] == 'X') {
+                    sum += 90;
+                    i--;
+                    continue;
+                } else if(s[i] == 'L' && s[i-1] == 'X') {
+                    sum += 40;
+                    i--;
+                    continue;
+                } else if(s[i] == 'D' && s[i-1] == 'C') {
+                    sum += 400;
+                    i--;
+                    continue;
+                } else if(s[i] == 'M' && s[i-1] == 'C') {
+                    sum += 900;
+                    i--;
+                    continue;
+                }
+            }
+            
+
+            if(s[i] == 'I') sum += 1;
+            else if(s[i] == 'V') sum += 5;
+            else if(s[i] == 'X') sum += 10;
+            else if(s[i] == 'L') sum += 50;
+            else if(s[i] == 'C') sum += 100;
+            else if(s[i] == 'D') sum += 500;
+            else if(s[i] == 'M') sum += 1000;
+        }
+        return sum;
+    }
+};
+
+*/
