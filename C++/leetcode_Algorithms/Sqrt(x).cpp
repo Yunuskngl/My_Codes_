fb7d9a9c853096ee7d9580a8aa943054b2abb1f0
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-08-12 02:32:10 +0300

    Update

---
+++ C++/leetcode_Algorithms/Sqrt(x).cpp
@@ -0,0 +1,13 @@
+/*SOLUTION:
+
+//https://leetcode.com/problems/sqrtx/description/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int a = sqrt(x);
+        return a;
+    }
+};
+
+*/
