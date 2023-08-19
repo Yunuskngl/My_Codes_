038c25ad095cd5a194f0d643eb9aa21ffe31f37f
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-08-19 19:03:36 +0300

    Update

---
+++ C++/leetcode_Algorithms/pow(x,n).cpp
@@ -0,0 +1,12 @@
+//SOLUTION:
+
+//https://leetcode.com/problems/powx-n/description/
+
+/*class Solution {
+public:
+    double myPow(double x, int n) {
+        double sum ; 
+        sum = pow(x,n);
+        return sum;
+    }
+};*/
