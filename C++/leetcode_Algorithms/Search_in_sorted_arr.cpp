97ac71561d6e60901efb10f681511a591ac5fc09
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-08-19 18:50:27 +0300

    uPdAtE

---
+++ C++/leetcode_Algorithms/Search_in_sorted_arr.cpp
@@ -0,0 +1,17 @@
+//SOLUTİON:
+
+//https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+/*class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.size() == 0) return 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == target){
+                return i;
+            }
+            
+        }
+        return -1;
+    }
+};*/
