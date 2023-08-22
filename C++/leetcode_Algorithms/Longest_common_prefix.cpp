34779b85fcd7bea2b00d5559378c4873f0db1651
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-08-22 12:16:16 +0300

    Update

---
+++ C++/leetcode_Algorithms/Longest_common_prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+    unordered_map<char,int>mp;
+    string s;
+    int i,j;
+    for(i=0;i<strs.size();i++){
+        for(j=0;j<strs[i].size();j++){
+            mp[strs[i][j]]++;
+        }
+    }
+    for(auto it=mp.begin();it!=mp.end();it++){
+        if(it->second==strs.size()){
+            s+=it->first;
+        }
+    }
+    return s;
+    }
+};
