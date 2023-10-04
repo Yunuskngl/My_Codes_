e5d755d6d17a74c9443ea0ecb62b3b61aa7c75a0
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-10-04 18:31:34 +0300

    Update

---
+++ C++/leetcode_Algorithms/Find_min_in_rotated_sorted_Array.cpp
@@ -0,0 +1,14 @@
+//SOLUTION:
+
+//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+/*
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int a = nums[0];
+        return a;
+    }
+};
+*/
