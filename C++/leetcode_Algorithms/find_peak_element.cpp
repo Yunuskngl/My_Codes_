efaa1cd981db0da34903b5b1bcb7d95379279409
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-10-05 14:29:37 +0300

    Update

---
+++ C++/leetcode_Algorithms/find_peak_element.cpp
@@ -0,0 +1,20 @@
+//SOLUTION:
+
+//https://leetcode.com/problems/find-peak-element/
+
+/*
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int max = nums[0];
+        int index = 0;
+        for(int i= 1 ; i<nums.size();i++){
+            if(max < nums[i]){
+                max = nums[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+};
+*/
