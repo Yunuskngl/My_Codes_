49f37be714eb0255b406da1d269e0bb30140d52c
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-11-03 00:07:38 +0300

    Update

---
+++ C++/leetcode_Algorithms/deneme.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+void sum() {
+    std :: cout << "abscsadadasdas" << std :: endl;
+    printf("%d", 5);
+}
+int main() {
+    
+    std :: cout << "Hello World!" << std :: endl;
+    std :: cout << "Hello World!" ;
+    sum();
+    return 0;
+}
+
+
