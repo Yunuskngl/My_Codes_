1e26fe7868eef5d14e3762350b85dfe628371dd7
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-11-09 16:43:18 +0300

    Update

---
+++ C++/leetcode_Algorithms/Last_Visited_Integers.cpp
@@ -0,0 +1,27 @@
+//SOLUTION:
+
+//https://leetcode.com/problems/last-visited-integers/
+
+/*
+class Solution {
+public:
+   
+    vector<int> lastVisitedIntegers(vector<string>& words) {
+        vector<int> nums;
+        vector<int> result;
+        int k = 0;
+        for (const string& word : words) {
+            if (word == "prev") {
+                k++;
+                if (k > nums.size()) result.push_back(-1);
+                else result.push_back(nums[nums.size() - k]);
+            }
+            else {
+                k = 0;
+                nums.push_back(stoi(word));
+            }
+        }
+        return result;
+    }
+};
+*/
