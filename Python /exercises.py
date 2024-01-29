be99817da0a9fb8a7fce8ab58ae21361935f13ee
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2024-01-29 17:51:17 +0300

    Update

---
+++ Python /exercises.py
@@ -0,0 +1,26 @@
+#is prime number
+"""
+def is_asal(a):
+    if (a <= 0): return False
+    if (a == 1): return False
+    if (a == 2): return True
+    for i in range(2, a):
+        if (a % i == 0): return False
+    return True
+
+while True:
+    a = int(input("input a number: "))
+    if (is_asal(a)):
+        print(a, "is a prime number")
+    else:
+        print(a, "is not a prime number")
+
+"""
+#--------------------------------------------------------
+
+#find divisors of a number
+
+def 
+
+
+
