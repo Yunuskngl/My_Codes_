6f29a59a01c030a675da290fd1ca8d965082ec9c
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2024-01-30 01:47:17 +0300

    Update

--- C++/Algo_Cpp_Education/Lecture7.cpp
+++ C++/Algo_Cpp_Education/Lecture7.cpp
@@ -110,3 +110,12 @@ int main() {
 }
 
 */
+
+int fibonacci_dynamic(int n) {
+    vector<int>fib(n + 1);
+    fib[0] = 0; fib[1] = 1;
+    for(int i = 2; i <= n; ++i){
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
+}
--- Python /exercises.py
+++ Python /exercises.py
@@ -43,3 +43,81 @@ print(result)
 
 #--------------------------------------------------------
 
+#is perfect numbers
+
+"""
+import numpy as np
+
+def divisors(num):
+    arr = np.array([1])
+    for i in range(2,num):
+        if num%i == 0 : arr = np.append(arr,i)
+       
+        
+    return arr
+
+
+
+a = int(input("input a number:"))
+result = divisors(a)
+print(result)
+sum = 0
+for i in range(0,len(result)):
+    sum += result[i]
+
+if sum == a : print("this is perfect number")
+else : print("this is not perfect number")
+"""
+#--------------------------------------------------------
+
+#Between 1-1000 of perfect numbers
+
+"""
+import numpy as np
+
+def is_perfect(num):
+    sum = 0
+    for i in range(1,num):
+        if num%i == 0 : sum+=i
+    
+    if sum != num : return False
+    return True
+    
+        
+for i in range(0,1000):
+    if(is_perfect(i) == True) : print(i) 
+"""
+#--------------------------------------------------------
+
+#find random number
+"""
+import random
+import time
+
+num = random.randint(1,40)
+predict = 7
+
+while True : 
+    prenum = int(input("input number:"))
+    
+    if (prenum < num) : 
+        print("please predict more than bigger number")
+        predict = predict - 1
+        time.sleep(1)
+    elif (prenum > num) : 
+        print("please predict more than lower number")
+        predict = predict - 1
+        time.sleep(1)
+    else :
+        time.sleep(2)
+        print("success")
+        break
+    if predict == 0 : 
+        print("fail")
+        break 
+    
+"""
+        
+#--------------------------------------------------------
+
+
