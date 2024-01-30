45e9b59404723973a25aa98308dda94a80ba3191
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2024-01-30 22:01:38 +0300

    Update

---
+++ Python /object_oriented_prgramming.py
@@ -0,0 +1,15 @@
+#class 
+
+class Araba():
+    model = "BMW"
+    colour = "Gümüş"
+    horsePower = 240
+    silindir = 8
+    
+
+araba1 = Araba()
+
+print(araba1.model)
+print(araba1.colour)
+print(araba1.horsePower)
+print(araba1.silindir)
