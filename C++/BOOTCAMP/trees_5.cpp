2aed3f6888bd8e1df78425d7eef9e48ea33ab024
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2024-02-13 05:46:10 +0300

    Update

--- C++/BOOTCAMP/trees_5.cpp
+++ C++/BOOTCAMP/trees_5.cpp
@@ -14,40 +14,41 @@ class BinarySearchtree{
     public:
     BinarySearchtree() : root(NULL) {};
 
-    TreeNode* insert(TreeNode *start , int data){
+    TreeNode* insert(TreeNode *start , int data){        //bu şekilde eleman ekleme işlemini gerçekleştirebilirsiniz.
         if(start){
             if(data < start->value) 
-                start->left = insert(start->left,data);
+                start->left = insert(start->left,data);    //eğer eklencek değer start value değerinden küçükse sola eklenir.
             else 
-                start->right = insert(start->right,data);
+                start->right = insert(start->right,data);  //büyükse sağa eklenir.
         }
+        else
+            return new TreeNode(data);
+        return start;
     }
-
-    int depth(TreeNode* start ,TreeNode* finish){        
+    int depth(TreeNode* start ,TreeNode* finish){       
         if(start){
             if(start == finish){
                 return 0;
             }
-            int left = depth(start->left,finish);         
+            int left = depth(start->left,finish);          
             int right = depth(start->right,finish);        
             return 1 + max(left,right);
         }
         else
             return INT_MIN;                                  
+            
     }
-
 
 };
 
 int main(){
     TreeNode* root;
     root = new TreeNode(30);
-    root->left = new TreeNode(25);
-    root->left->right = new TreeNode(27);
-    root->left->left = new TreeNode(23);
-    root->right  = new TreeNode(34);
-    root->right->right = new TreeNode(35);
-    root->right->right->right = new TreeNode(36);
     BinarySearchtree tree;
-    cout<< tree.depth(root,root->right->right->right)<< endl;
+    tree.insert(root,5);
+    tree.insert(root,35);
+    tree.insert(root->left,10);
+    tree.insert(root->left->right,15);
+    cout<< tree.depth(root,root->left->right)<< endl;
+    return 0;
 }
