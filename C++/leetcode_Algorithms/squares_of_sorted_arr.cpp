27fa89ae32a76829ad8a1f249cd090fc63d606fd
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2024-03-02 06:12:10 +0300

    Update

---
+++ C++/leetcode_Algorithms/squares_of_sorted_arr.cpp
@@ -0,0 +1,18 @@
+//SOLUTION : 
+
+//https://leetcode.com/problems/squares-of-a-sorted-array/
+
+/*
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int>v;
+        for(int i=0;i<nums.size();i++){
+            int temp = nums[i];
+            v.push_back(temp*temp);
+        }
+        sort(v.begin(),v.end());
+        return v;
+    }
+};
+*/
