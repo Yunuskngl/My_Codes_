d3fd362be27295c8cdd86c834f2b33dd61643845
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2024-03-10 13:25:07 +0300

    Update

---
+++ C++/leetcode_Algorithms/Sort_integers_by_the_number_of_1_bits.cpp
@@ -0,0 +1,20 @@
+//SOLUTION:
+
+//https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/
+
+/*
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [](int a, int b){
+            int countA = __builtin_popcount(a);
+            int countB = __builtin_popcount(b);
+            if(countA == countB){
+                return a < b;
+            }
+            return countA < countB;
+        });
+        return arr;
+    }
+};
+*/
