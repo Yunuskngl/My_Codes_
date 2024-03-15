5ee8a29d2fcf98b5a9788c5c630804cb9a09df8c
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2024-03-15 21:32:05 +0300

    Update

---
+++ C++/leetcode_Algorithms/binary_subarrays_with_sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        int sum = 0;
+        unordered_map<int, int> count;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            count[sum]++;
+            sum += nums[i];
+            ans += count[sum - goal];
+        }
+        return ans;
+    }
+};
