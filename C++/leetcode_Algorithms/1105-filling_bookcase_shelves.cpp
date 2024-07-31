fcfe5883f282779b7cc14c72f63ded64aed45268
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2024-07-31 23:04:45 +0300

    update

---
+++ C++/leetcode_Algorithms/1105-filling_bookcase_shelves.cpp
@@ -0,0 +1,28 @@
+//SOLUTION:
+
+//https://leetcode.com/problems/filling-bookcase-shelves/
+
+
+class Solution {
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        int kitap = books.size();
+        vector<int> dp(kitap + 1, 0);
+        dp[0] = 0;
+        for (int i = 1; i <= kitap; ++i) {
+            int width = 0;
+            int height = 0;
+            dp[i] = INT_MAX;
+            for (int j = i - 1; j >= 0; --j) {
+                width += books[j][0];
+                if (width > shelfWidth) {
+                    break;
+                }
+                height = max(height, books[j][1]);
+                dp[i] = min(dp[i], dp[j] + height);
+            }
+        }
+        return dp[kitap];
+    }
+    
+};
